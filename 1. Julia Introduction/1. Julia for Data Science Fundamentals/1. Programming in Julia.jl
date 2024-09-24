0229f62d123ce7ee1c8c0699eb163258ef5c708f
Shahriar Kabir <110348315+shahriar0999@users.noreply.github.com>
2024-09-24 17:45:20 +0600

    complete first chapter code

---
+++ 1. Julia Introduction/1. Julia for Data Science Fundamentals/1. Programming in Julia.jl
@@ -0,0 +1,35 @@
+## 2. Programming in Julia ##
+
+8 + 42
+
+## 3. The println() Command ##
+
+println(40 + 4)
+println(200 - 25)
+println(14 + 3)
+
+## 4. Julia Syntax ##
+
+println(30 + 10 + 40)
+println(4)
+println(-3)
+
+## 5. Computer Programs ##
+
+println(34 + 16)
+println(34)
+println(-34)
+
+## 6. Code Comments ##
+
+# INITIAL CODE
+
+println(34 + 16)
+println(34)
+println(-34)
+
+## 7. Arithmetical Operations ##
+
+println(16 * 10)
+println(48 / 5)
+println(5^3)
